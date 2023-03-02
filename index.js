51b2a07b0c9332ed2b1357aa1edde834644a903d
SirIsaacNeutron <dbadir@cox.net>
2023-03-02 00:16:58 -0800

    Add password confirmation validation

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const zipCodeInput = document.getElementById("zip-code")
 const zipCodeError = document.querySelector("#zip-code + .error-msg")
 
 const passwordInput = document.getElementById("password")
-const passwordError = document.querySelector("#password + error-msg")
+const passwordError = document.querySelector("#password + .error-msg")
+
+const passwordConfirmInput = document.getElementById("password-confirmation")
+const passwordConfirmError = document.querySelector("#password-confirmation + .error-msg")
 
 const form = document.querySelector(".basic-form")
 
+let doPasswordsMatch = false
+
 emailInput.addEventListener("input", () => {
     if (emailInput.validity.valid) {
+        // Hide the error message
         emailError.textContent = ""
         emailError.className = "error-msg"
     } else {
@@ -35,8 +41,43 @@ zipCodeInput.addEventListener("input", () => {
     }
 })
 
+// passwordInput.addEventListener("input", () => {
+//     // Mismatching passwords
+//     if (passwordInput.value !== passwordConfirmInput.value) {
+//         passwordError.textContent = "Your passwords don't match."
+//         passwordError.className = "error-msg active"
+//     }
+//     else {
+//         passwordError.textContent = ""
+//         passwordError.className = "error-msg"
+//     }    
+// })
+
+passwordConfirmInput.addEventListener("input", () => {
+    if (passwordInput.value !== passwordConfirmInput.value) {
+        passwordConfirmError.textContent = "Your passwords don't match."
+        passwordConfirmError.className = "error-msg active"
+        
+        doPasswordsMatch = false
+    }
+    else {
+        passwordConfirmError.textContent = ""
+        passwordConfirmError.className = "error-msg"
+
+        doPasswordsMatch = true
+    }
+})
+
 form.addEventListener("submit", (e) => {
     if (!emailInput.validity.valid) {
+        e.preventDefault()
+    }
+
+    if (!zipCodeInput.validity.valid) {
+        e.preventDefault()
+    }
+
+    if (!doPasswordsMatch) {
         e.preventDefault()
     }
 })
